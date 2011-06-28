9be5d1b266faa5108a49febda3c8c2bef02adbd2
Nitin Bansal <nitinbansal2486@gmail.com>
2011-06-28 15:12:19 +0530

    Quadrate added

--- SPOJ/QUADRATE/roots.cpp
+++ SPOJ/QUADRATE/roots.cpp
@@ -6,7 +6,7 @@
 
 * Creation Date : 12-05-2011
 
-* Last Modified : Monday 27 June 2011 05:02:42 PM IST
+* Last Modified : Tuesday 28 June 2011 03:10:01 PM IST
 
 * Created By : Nitin
 
@@ -34,40 +34,70 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 typedef long long ll;
 typedef unsigned long long ull;
 
+void Print(int a, int b, int c){
+    ll D;
+    
+    D = b*b - 4*a*c;
+    if(DEBUG) std::cout << a << "()" << b << "()" << c << "=>" << D << "  ";
+    if(D == 0){ printf("Equal roots.\n"); }
+    else if(D < 0){ printf("Imaginary roots.\n"); }
+    else{ printf("Distinct real roots.\n"); }
+}
+
 int main(){
+//    freopen("input.txt","r",stdin);
+
     int num_test_cases,a,b,c,t;
     char inp[MAX_SIZE];
-    ll D;
     char *ptr;
     scanf("%d",&num_test_cases); getchar();
     for(int i = 0; i < num_test_cases; i++){
         scanf("%[^\n]",inp); getchar();
-        a = b = c = 1;
+        a = b = c = 0;
         ptr = inp;
-        if(*ptr != 'x'){ 
-            sscanf(ptr,"%d",&a);
+
+        if(*ptr == '-'){ a = -1; ptr++; }
+        else { a = 1; }
+
+        if(*ptr != 'x'){
+            sscanf(ptr,"%d",&t);
+            a=a*t;
             while(*ptr!= 'x') ptr++;
         }
         ptr++; ptr++; ptr++;
         
         if(*ptr == '-'){ b = -1; }
+        else if(*ptr == '+'){ b = 1; }
+        else{
+            Print(a,0,0);
+            continue; 
+        }
+
         ptr++;
         if(*ptr != 'x'){ 
             sscanf(ptr,"%d",&t);
             b=b*t;
-            while(*ptr!= 'x') ptr++;
+            while(*ptr >= '0' && *ptr <= '9') ptr++;
+            if(*ptr != '*'){
+               c = b; b = 0;
+               Print(a,b,c);
+               continue;
+            }
+            else{
+              ptr++; 
+            }
         }
         ptr++;
 
         if(*ptr == '-'){ c = -1; }
-        ptr++;        
+        else if(*ptr == '+'){ c = 1; }
+        else{
+            Print(a,b,c);
+            continue; 
+        }
+        ptr++;
         sscanf(ptr,"%d",&t);
         c=c*t;
-        
-        if(DEBUG) std::cout << a << "()" << b << "()" << c << std::endl;
-        D = b*b - 4*a*c;
-        if(D == 0){ printf("Equal roots.\n"); }
-        else if(D < 0){ printf("Imaginary roots.\n"); }
-        else{ printf("Distinct real roots.\n"); }
+        Print(a,b,c);
     }
 }
