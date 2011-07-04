9aab2df25460fb04791183974f9c24e3c7a227f9
Nitin Bansal <nitinbansal2486@gmail.com>
2011-07-04 18:29:15 +0530

    Problems related to Trees

---
+++ Tree/distK.cpp
@@ -0,0 +1,107 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : distK.cpp
+
+* Purpose :
+
+* Creation Date : 12-05-2011
+
+* Last Modified : Monday 04 July 2011 04:56:32 PM IST
+
+* Created By : Nitin
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+
+/*STL *algorithm* */
+#include <queue>
+#include <stack>
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+
+#define DEBUG 1
+
+struct node{
+  public:
+   int data;
+   struct node *left ;
+   struct node *right;
+     node(){
+        data = -1;
+        left = right = NULL;
+     }
+};
+/*
+ * GLOBAL variable representing the root
+ *  node of tree
+ */
+node * root = NULL;
+
+
+/*
+ * @desc: 
+ *   insert function for BST **
+ * @param: 
+ *   value to be inserted
+ */
+void insert ( int value ){
+  if (root == NULL) {
+    root = new node();
+    root->data = value;
+    return;
+  }
+
+  node * curr = root;
+  node * prev = curr;
+  while(1){
+    prev = curr;
+    curr = curr->data >= value ? curr->left : curr->right;
+    if(curr == NULL){
+       node * n = new node();
+       n->data  = value;
+       if( value < prev->data ) prev->left = n; 
+       else prev->right = n;
+       break;
+    }
+  }
+  return;
+}
+
+void print_dist(node *temp, int value){
+    if(temp == NULL){
+        std::cout << "no node found" << std::endl;
+        return;
+    }
+   
+    if(value == 0){
+        std::cout << temp->data << std::endl;
+        return;
+    }
+
+    print_dist(temp->left, value-1);
+    print_dist(temp->right, value-1);
+    return;
+}
+
+int main(){
+  int value;
+  std::cout << "input value to insert (( untill -1 )) " << std::endl;
+  while(1){
+    std::cin >> value;
+    if(value == -1) break;
+    insert(value);
+  }
+
+  std::cout << "dist from root (( untill -1 )) " << std::endl;
+  while(1){
+    std::cin >> value;
+    if(value == -1) break;
+    print_dist(root, value);
+  }
+}
