73aca0e26d48dc35c838879eab85ae1ae1b95850
Nitin <nitinbansal2486@gmail.com>
2012-05-03 19:14:53 +0530

    Linkedlist: Add method to remove loop from linkedlist

--- LinkedList/LList.cpp
+++ LinkedList/LList.cpp
@@ -1,17 +1,3 @@
-/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
-
-* File Name : LList.cpp
-
-* Purpose :
-
-* Creation Date : 12-05-2011
-
-* Last Modified : Friday 13 May 2011 07:58:00 PM IST
-
-* Created By : Nitin
-
-_._._._._._._._._._._._._._._._._._._._._.*/
-
 #include <iostream>
 #include <cmath>
 
@@ -33,7 +19,7 @@ struct node {
     node(){
        data = -1;
        next = NULL;
-    } 
+    }
 };
 
 class linkedlist{
@@ -44,93 +30,145 @@ class linkedlist{
   }
   void Insert(int);
   void InsertSort(int);
-  void CreateLoop();
   void Print();
+  void CreateLoop();
   void DetectAndRemoveLoop();
+  void RemoveLoop(node *temp);
 };
 
 void linkedlist::Insert(int value){
     node *nn = new node();
     nn->data = value;
-    node *temp;
-    
-    if(!root) { root = nn; }
-    else{
-       temp = root;
-       while(temp->next){ temp = temp->next; }
-       temp->next = nn;
+
+    if(root == NULL){
+        root = nn;
+        return;
     }
+
+    node *temp = root;
+    while(temp->next){
+        temp = temp->next;
+    }
+    temp->next = nn;
+
     return;
 }
 
 void linkedlist::Print(){
-   node *temp = root;
-   while(temp){
-     std::cout << "Data : " << temp->data << std::endl;
-     temp = temp->next; 
-   }
+    node *temp = root;
+    std::cout << "LL=> ";
+    while(temp){
+        std::cout << temp->data << " ";
+        temp = temp->next;
+    }
+    std::cout << std::endl;
 }
 
 void linkedlist::CreateLoop(){
-   node *temp = root; // assuming there are few nodes in ll
-   while(temp->next){ temp = temp->next; }
+   node *temp = root;
+   while(temp->next){
+       temp = temp->next;
+   }
    temp->next = root->next->next;
 }
 
 void linkedlist::DetectAndRemoveLoop(){
-   node *temp1, *temp2;
-   temp1 = root; temp2 = root;
+   node *temp1 = root, *temp2 = root;
+
    while(1){
-     if(temp1 == NULL){ std::cout << "NULL ptr@1 found " << std::endl; return; }
-     else temp1 = temp1->next;
-     
-     if(temp2 == NULL){ std::cout << "NULL ptr@2 found " << std::endl; return; }
-     else temp2 = temp2->next;
-     
-     if(temp2 == NULL){ std::cout << "NULL ptr@3 found " << std::endl; return; }
-     else temp2 = temp2->next;
-     
+     if(temp1 == NULL){
+         std::cout << "NULL ptr@1 found " << std::endl;
+         return;
+     }
+     temp1 = temp1->next;
+
+     if(temp2 == NULL){
+         std::cout << "NULL ptr@2 found " << std::endl;
+         return;
+     }
+     temp2 = temp2->next;
+
+     if(temp2 == NULL){
+         std::cout << "NULL ptr@3 found " << std::endl;
+         return;
+     }
+     temp2 = temp2->next;
+
      if(temp1 == temp2){
-         std::cout << "Loop Detected at " << temp1->data << std::endl; 
-         return;      
+         std::cout << "Loop Detected" << std::endl;
+         RemoveLoop(temp1);
+         return;
      }
    }
+}
+
+void linkedlist::RemoveLoop(node *temp){
+    int count = 1;
+
+    node *iter = temp->next;
+    while(iter != temp){
+        iter = iter->next;
+        count++;
+    }
+
+    node *start1 = this->root, *start2 = this->root;
+    while(count-- != 0){
+        start1 = start1->next;
+    }
+
+    while(start2->next != start1->next){
+        start1 = start1->next;
+        start2 = start2->next;
+    }
+
+    if(DEBUG){
+        std::cout << start2->next->data << " " << start1->next->data << std::endl;
+    }
+
+    std::cout << "Loop Removed" << std::endl;
+    start1->next = NULL;
 }
 
 void linkedlist::InsertSort(int value){
     node *nn = new node();
     nn->data = value;
-    node *temp, *prev;
-    
-    if(!root) { root = nn; }
-    else{
-       temp = root; prev = NULL;
-       while(temp && temp->data < value){ prev = temp; temp = temp->next; }
-       if(prev == NULL){ nn->next = temp; root = nn; }
-       else{
-          prev->next = nn;
-          nn->next   = temp;
-       }
+
+    if(root == NULL){
+        root = nn;
+        return;
     }
+
+    node *temp = root, *prev = NULL;
+    while(temp && temp->data < value){
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if(prev == NULL){
+        nn->next = temp;
+        root = nn;
+        return;
+    }
+
+    prev->next = nn;
+    nn->next = temp;
     return;
 }
 
 int main(){
-   linkedlist *ll = new linkedlist(); 
-    
+   linkedlist *ll = new linkedlist();
+
    int num;
    std::cin >> num;
    while(num!=-1){
       ll->InsertSort(num);
-      ll->Print();
       std::cin >> num;
    }
-   //ll->Print();
-   /*
+   ll->Print();
+
    ll->CreateLoop();
-   ll->Print(); // while(1) if loop is present in ll
    ll->DetectAndRemoveLoop();
-   */
-    
+   ll->Print();
+
    while(1){ continue; }
 }
