111e352252795c536b9a60b062156a7bc236be06
Nitin <nitinbansal2486@gmail.com>
2012-05-17 19:42:32 +0530

    wuforums: Another method to find missing elements

--- wuforums/MissingElements.cpp
+++ wuforums/MissingElements.cpp
@@ -27,21 +27,29 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #define DEBUG 1
 
 void find_missing_elements(int *arr, int len){
-   int i = 0;
-   int count_sw = 0;
-   while(i < len){
-      if(arr[i] == -1){ i++; continue; }
-      else if(arr[i] != i+1){ //swap arr[i] with arr[arr[i]-1]]
-         int temp = arr[i];
-         arr[i]   = arr[temp-1];
-         arr[temp-1] = temp;
-         count_sw++;
-         if(count_sw > len-1){ i++; count_sw = 0; }
-      }
-      else{
-        i++; count_sw = 0;
-      }
-   }
+    int i = 0;
+    int count_sw = 0;
+    while(i < len){
+        int temp = arr[i];
+
+        if(temp == -1){
+            i++;
+            continue;
+        }
+        else if(temp != i+1){
+            arr[i]   = arr[temp-1];
+            arr[temp-1] = temp;
+            count_sw++;
+            if(count_sw > len-1){
+                i++;
+                count_sw = 0;
+            }
+        }
+        else{
+            i++;
+            count_sw = 0;
+        }
+    }
 }
 
 int main(){
@@ -50,18 +58,12 @@ int main(){
 
   int SIZE = num_te + num_me;
   int *arr = (int *)malloc(SIZE*sizeof(int));
-
   for(int i = 0; i < SIZE; i++){
     arr[i] = -1;
   }
   
   for(int i = 0; i < num_te; i++){
     std::cin >> arr[i];
-  }
-  std::cout << std::endl;
-   
-  for(int i = 0; i < SIZE; i++){
-    std::cout << arr[i];
   }
   std::cout << std::endl;
     
