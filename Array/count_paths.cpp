061a298a94472ea2b008bb8f25724779af79e1e3
Nitin Bansal <nitinbansal2486@gmail.com>
2017-02-09 17:22:38 +0000

    Create count_paths.cpp

---
+++ Array/count_paths.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int count(int posx, int posy, int M, int N, bool **grid){
+    if(posx < 0 || posx >= M || posy < 0 || posy >= N ||
+       grid[posx][posy]){
+        return 0;
+    }
+
+    grid[posx][posy] = true;
+    int subtotal = count(posx, posy+1, M, N, grid);
+    subtotal += count(posx, posy-1, M, N, grid);
+    subtotal += count(posx+1, posy, M, N, grid);
+    subtotal += count(posx-1, posy, M, N, grid);
+    grid[posx][posy] = false;
+
+    return subtotal;
+}
+
+int main(){
+    int M, N;
+    std::cin >> M >> N;
+
+    bool **grid;
+    grid = (bool**)malloc(sizeof(char*)*M);
+    for(int i = 0; i < M; i++){
+        grid[i] = (bool*)malloc(sizeof(bool)*N);
+    }
+
+
+    total += count(0, 0, M, N, grid);
+
+    std::cout << total / 4 << std::endl;
+}
